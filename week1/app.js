d29aad8b1393db973e686224313377b2875eabf1
MPerson92 <76800220+MPerson92@users.noreply.github.com>
2021-06-09 15:01:08 -0400

    calc

---
+++ week1/app.js
@@ -0,0 +1,7 @@
+const calculator = require('./math');
+
+console.log(calculator.add(5, 6));
+console.log(calculator.difference(8, 7));
+console.log(calculator.divide(20, 5));
+console.log(calculator.multiply(20, 30));
+
